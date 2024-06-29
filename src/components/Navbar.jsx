7411eca319ee3b2ba720653fe8afdf0a158034c1
manavjain179 <manavjain179@gmail.com>
2024-06-29 11:46:40 -0500

    Added animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,18 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/manavjain179/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/manavjain179/" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/manavjain179/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
